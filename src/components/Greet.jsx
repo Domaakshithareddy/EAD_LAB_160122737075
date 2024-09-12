bb13e13fc8b941a7d302c4b777b1e3a917125676
Domaakshithareddy <157153477+Domaakshithareddy@users.noreply.github.com>
2024-09-12 15:57:15 +0530

    Delete src directory

--- src/components/Greet.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-function Greet ({name}) {
-    return(
-        <h1>Hello, {name}</h1>
-    );
-};
-export default Greet;
--- src/components/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const HelloWorld = () => {
-    return (
-    <div>
-        <h1>Hello Akshitha</h1>
-    </div>
-    );
-};
-export default HelloWorld;
